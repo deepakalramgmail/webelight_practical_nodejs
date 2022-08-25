e01d9fb10ad9add60d9faa30b89f70a5a2d9157d
Deepak Alram <deepakalram10@gmail.com>
2022-08-26 01:39:45 +0530

    auth module implemented

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const routes = require('express').Router();
-
+const { routeMiddleWares } = require('../helper/middlewares');
 /* controller */
 const auth = require('../controller/auth');
 /* controller */
 
-routes.get('/', auth.test);
+routes.post('/auth/register', routeMiddleWares, auth.register);
+routes.post('/auth/login', auth.login);
 
 module.exports = routes;
